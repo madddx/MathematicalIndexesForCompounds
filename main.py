db41f61deb673a3cbe1e4a8e6a0a04d65ad09c6c
AgiGames <155605209+AgiGames@users.noreply.github.com>
2025-06-17 16:17:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import helper
 import parser
 import topology
+from chemspipy import ChemSpider
 
-file_path = r"D:\Education\Code Bases\Python\StdInChlParser\std_in_chl_string3.text"
-std_in_chl_string = helper.std_in_chl_file_reader(file_path)
+cs = ChemSpider('k66XWXT4eY8blIm8nZprn1G7HBCqHvD4Cd0Oe0Y1')
+
+# file_path = r"D:\Education\Code Bases\Python\StdInChlParser\std_in_chl_string3.text"
+# std_in_chl_string = helper.std_in_chl_file_reader(file_path)
+# print(std_in_chl_string)
+
+std_in_chl_string = cs.search('ethanol')[0].inchi
 print(std_in_chl_string)
-ids_and_elements = parser.parse_std_in_chl_string(std_in_chl_string)
 
+ids_and_elements = parser.parse_std_in_chl_string(std_in_chl_string)
 print(topology.find_basic_topology(ids_and_elements=ids_and_elements))
